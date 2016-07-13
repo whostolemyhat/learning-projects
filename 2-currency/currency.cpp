d68fd09fed1d45adf98f6df828a4d1b33929c16a
James Tease <james@jamestease.co.uk>
2016-07-13 15:48:42 +0100

    Refactor

--- 2-currency/currency.cpp
+++ 2-currency/currency.cpp
@@ -1,24 +1,74 @@
 // Develop a programme to convert currency X to currency Y and vice versa
 // £1 = £1.32 lol
 #include <iostream>
+#include <iomanip>
+#include <string>
 
 using std::cout;
 using std::cin;
 using std::endl;
+using std::string;
 
-const float DOLLAR_CONVERSION = 1.32;
+const float POUNDS_TO_DOLLARS = 1.32;
+const float DOLLARS_TO_POUNDS = 0.75;
 
-int main() {
-  cout << "Enter amount in £:";
-
-  float pounds;
-  cin >> pounds;
-
-  if(!cin) {
-    cout << "Enter a number" << endl;
+float convert(float amount, string type) {
+  if(type == "pounds") {
+    return amount * POUNDS_TO_DOLLARS;
   }
 
-  cout << "£" << pounds << " is $" << pounds * DOLLAR_CONVERSION << endl;
+  return amount * DOLLARS_TO_POUNDS;
+}
+
+int main() {
+  // todo: do while
+  cout << "Choose from the following options (type number to begin):" << endl;
+  cout << "(1) Pounds to dollars" << endl;
+  cout << "(2) Dollars to pounds" << endl;
+
+  int choice;
+  cin >> choice;
+
+  if(!cin) {
+    cout << "Please type the number of your choice";
+    return 1;
+  }
+
+  string type;
+  string sign;
+  string convertedSign;
+  float amount;
+
+  switch(choice) {
+    case 1:
+      cout << "Enter amount in £";
+      type = "pounds";
+      sign = "£";
+      convertedSign = "$";
+      break;
+
+    case 2:
+      cout << "Enter amount in $";
+      type = "dollars";
+      sign = "$";
+      convertedSign = "£";
+      break;
+
+    default:
+      cout << "Please pick an option from the list";
+      return 1;
+  }
+
+  cin >> amount;
+  if(!cin) {
+    cout << "Enter a number" << endl;
+
+    return 1;
+  }
+
+  cout << sign << amount << " is "
+    << convertedSign << std::fixed << std::setprecision(2)
+    << convert(amount, type) << endl;
 
   return 0;
 }
