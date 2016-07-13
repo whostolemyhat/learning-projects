8186f12db55dab725abf66ec9172dd0cbd40fa46
James Tease <james@jamestease.co.uk>
2016-07-13 13:27:12 +0100

    Cpp factorial

--- 1-factorial/cpp/factorial.cpp
+++ 1-factorial/cpp/factorial.cpp
@@ -4,7 +4,25 @@
 // 4! = 4 + 3 + 2 + 1 = 10
 #include <iostream>
 
+using std::cout;
+using std::cin;
+using std::endl;
+
+int factorial(int num) {
+  if (num < 1) {
+    return 0;
+  }
+
+  return num + factorial(num - 1);
+}
+
 int main() {
-  std::cout << "Hello!" << std::endl;
+  cout << "Enter a number: ";
+
+  int num;
+  cin >> num;
+
+  cout << factorial(num);
+
   return 0;
 }
