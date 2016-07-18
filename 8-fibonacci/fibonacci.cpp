a6971908ba282744df58e41231482b31539b9cb2
James Tease <james@jamestease.co.uk>
2016-07-18 16:21:35 +0100

    Fib cpp

--- 8-fibonacci/fibonacci.cpp
+++ 8-fibonacci/fibonacci.cpp
@@ -19,20 +19,18 @@ int main() {
 
   // std::cout << fib(5);
   int target = 5;
-  int first = 0;
-  int second = 0;
+  int first = 1;
+  int second = 1;
   int temp;
   for(int i = 0; i < target; i++) {
     if(i == 0) {
       std::cout << first << std::endl;
-      first = 1;
-      break;
+      continue;
     }
 
     if(i == 1) {
       std::cout << second << std::endl;
-      second = 1;
-      break;
+      continue;
     }
 
     std::cout << first + second << std::endl;
