bc947476bf4b343a5c7b602fa75ba6e0d8c49233
James Tease <james@jamestease.co.uk>
2016-07-18 15:53:32 +0100

    Fib cpp

--- 8-fibonacci/fibonacci.cpp
+++ 8-fibonacci/fibonacci.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 
 int fib(int x) {
-  if(x <= 1) {
+  std::cout << x << std::endl;
+  if(x < 2) {
     return 1;
   }
 
-  return fib(x - 1) + fib(x - 2);
+  return fib(x - 2) + fib(x - 1);
 }
 
 int main() {
