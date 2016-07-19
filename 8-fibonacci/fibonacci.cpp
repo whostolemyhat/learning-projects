cc95e2e52d7578e3bde786bb23a47fda1a8ddbcf
James Tease <james@jamestease.co.uk>
2016-07-19 09:32:08 +0100

    Fib cpp

--- 8-fibonacci/fibonacci.cpp
+++ 8-fibonacci/fibonacci.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 
 // this calcs nth fibonacci number
-// int fib(int x) {
-//   // std::cout << x << std::endl;
-//   if(x < 2) {
-//     return 1;
-//   }
+int fib(int x) {
+  // std::cout << x << std::endl;
+  if(x < 2) {
+    return 1;
+  }
 
-//   return fib(x - 2) + fib(x - 1);
-// }
+  return fib(x - 2) + fib(x - 1);
+}
 
 int main() {
   // int num;
@@ -22,30 +22,23 @@ int main() {
   // this prints out up to nth fibonacci
   int target = 5;
   int first = 0;
-  int second = 0;
-  // int temp;
+  int second = 1;
+
   int output = 0;
+  int i = 0;
   // for(int i = 0; i < target; i++) {
   while(output < target) {
-    if(output == 0) {
-      first++;
-    //   output = first;
-    //   continue;
+    if(output <= 1) {
+      output = i;
+    } else {
+      output = first + second;
+
+      first = second;
+      second = output;
     }
 
-    if(output == 1) {
-      second++;
-    //   std::cout << second << " ";
-    //   continue;
-    }
-
-    output = first + second;
     std::cout << output << " ";
-
-    // change vars
-    // temp = second;
-    first = second;
-    second = output;
+    i++;
   }
 
   return 0;
