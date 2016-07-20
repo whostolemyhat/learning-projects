5adc4eb5bc421b9f873aa3bc75bcd1de78f0bc23
James Tease <james@jamestease.co.uk>
2016-07-20 15:13:19 +0100

    Added test to rust

--- 8-fibonacci/fib/src/main.rs
+++ 8-fibonacci/fib/src/main.rs
@@ -1,15 +1,7 @@
 use std::io;
 
-fn main() {
-  println!("Enter a number:");
-
-  let mut input_text = String::new();
-
-  io::stdin()
-    .read_line(&mut input_text)
-    .expect("Failed to read input");
-
-  let target: u64 = input_text.trim().parse().expect("That's no number!");
+fn fib(target: u64) -> String {
+  let mut output_string = String::new();
 
   let mut first = 0;
   let mut second = 1;
@@ -27,8 +19,35 @@ fn main() {
     }
 
     if output < target {
-      println!("{}", output);
+      // println!("{}", output);
+      output_string = format!("{} {}", output_string, output);
     }
     i += 1;
   }
+
+  output_string.trim().to_string()
+}
+
+fn main() {
+  println!("Enter a number:");
+
+  let mut input_text = String::new();
+
+  io::stdin()
+    .read_line(&mut input_text)
+    .expect("Failed to read input");
+
+  let target: u64 = input_text.trim().parse().expect("That's no number!");
+
+  let sequence = fib(target);
+
+  println!("{}", sequence);
+}
+
+
+#[test]
+fn it_calculates_sequence() {
+  assert!(fib(5) == "0 1 1 2 3");
+  assert!(fib(50) == "0 1 1 2 3 5 8 13 21 34");
+  assert!(fib(0) == "");
 }
