144f608f9be4f9a3a7683d4aafc171dd87b0036c
James Tease <james@jamestease.co.uk>
2016-08-01 11:32:55 +0100

    starting cpp circle

--- 12-circle/circle.cpp
+++ 12-circle/circle.cpp
@@ -11,6 +11,10 @@ public:
   Circle(float area = 0, float radius = 0, float diameter = 0) :
     area(area), radius(radius), diameter(diameter) {}
 
+    static Circle Circle::from_radius(float radius) {
+      return Circle(M_PI * (radius * radius), radius, radius * 2);
+    }
+
   void print() {
     std::cout << "Circle(area: " << area
       << ", radius: " << radius
@@ -18,9 +22,6 @@ public:
   }
 };
 
-Circle Circle::from_radius(float radius) {
-  return Circle(M_PI * (radius * radius), radius, radius * 2);
-}
 
 int main() {
   std::cout << "Enter size: ";
