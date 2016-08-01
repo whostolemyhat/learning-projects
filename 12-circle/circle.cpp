9cf42b9da798655862516f4e0021a9d0368334c6
James Tease <james@jamestease.co.uk>
2016-08-01 11:38:13 +0100

    starting cpp circle

--- 12-circle/circle.cpp
+++ 12-circle/circle.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <math.h>
+#include <math.h> // M_PI
 
 class Circle {
 private:
@@ -11,9 +11,21 @@ public:
   Circle(float area = 0, float radius = 0, float diameter = 0) :
     area(area), radius(radius), diameter(diameter) {}
 
-    static Circle from_radius(float radius) {
-      return Circle(M_PI * (radius * radius), radius, radius * 2);
-    }
+  static Circle from_radius(float radius) {
+    return Circle(M_PI * (radius * radius), radius, radius * 2);
+  }
+
+  static Circle from_diameter(float diameter) {
+    float radius = diameter / 2.0;
+
+    return Circle(M_PI * (radius * radius), radius, diameter);
+  }
+
+  state Circle from_area(float area) {
+    float diameter = (area / M_PI).sqrt();
+
+    return Circle(area, diameter / 2.0, diameter);
+  }
 
   void print() {
     std::cout << "Circle(area: " << area
