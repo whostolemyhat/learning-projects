d05505b6f6b89e8c37d768ff9dc2edb5de6a71ad
James Tease <james@jamestease.co.uk>
2016-08-02 15:58:48 +0100

    Starting pointer

--- 14-pointers/pointers.cpp
+++ 14-pointers/pointers.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 
+void swap(int& a, int& b) {
+  a = a + b;
+  b = a - b;
+  a = a - b;
+}
+
 int main() {
   int a = 1;
   int b = 2;
   int c = 3;
 
-  a = a + b;
-  b = a - b;
-  a = a - b;
+  // a = a + b;
+  // b = a - b;
+  // a = a - b;
+
+  swap(a, b);
+  swap(b, c);
 
   std::cout << a << " " << b << std::endl;
 
