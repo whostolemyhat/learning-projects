d5c9ef215ba60ed044d4d800b339c6799eb27ace
James Tease <james@jamestease.co.uk>
2016-08-03 13:38:49 +0100

    Starting calc cpp

--- 14-calc/calc.cpp
+++ 14-calc/calc.cpp
@@ -1,3 +1,22 @@
+float divide(int first, int second) {}
+int mulitply(int first, int second) {}
+int add(int first, int second) {}
+int subtract(int first, int second) {}
+
+using namespace std;
+
 int main() {
+  cout << "Enter a sum: ";
+
+  string input;
+  cin >> input;
+
+  string[] operators = ["+", "-", "*", "/"];
+
+  // search for operator in string
+  for(auto op : operators) {
+    cout op << ": " << s.find(op) << endl;
+  }
+
   return 0;
 }
