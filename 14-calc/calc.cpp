9d48c4e43c8035d74acce73fd4d5218bbd18421d
James Tease <james@jamestease.co.uk>
2016-08-04 10:43:05 +0100

    Starting calc cpp

--- 14-calc/calc.cpp
+++ 14-calc/calc.cpp
@@ -30,42 +30,50 @@ int main() {
 
   std::cout << "Enter a sum: ";
 
-  string input;
-  getline(cin, input);
+  string op;
+  int x;
+  int y;
+
+  // string input;
+  stringstream data(cin);
+  // getline(cin, input);
+  data >> x >> op >> y;
+
+  std::cout << x << op << y << std::endl;
 
   string operators[] = {"+", "-", "*", "/"};
 
   // search for operator in string
   // if not in string, returns number greater than string length
   // can also use std::string::npos
-  for(auto op : operators) {
-    std::size_t found = input.find(op);
+  // for(auto op : operators) {
+  //   std::size_t found = input.find(op);
 
-    if(found != std::string::npos) {
-      // std::cout << op << ": " << input.find(op) << std::endl;
-      // split string at operator
-      // get first and second numbers
-      auto start = 0;
-      auto end = found;
-      int x = input.substr(start, end - start);
-      int y = input.substr(end + op.length(), input.length());
+  //   if(found != std::string::npos) {
+  //     // std::cout << op << ": " << input.find(op) << std::endl;
+  //     // split string at operator
+  //     // get first and second numbers
+  //     auto start = 0;
+  //     auto end = found;
+  //     int x = input.substr(start, end - start);
+  //     int y = input.substr(end + op.length(), input.length());
 
-      std::cout << (int)x << std::endl;
-      std::cout << (int)y << std::endl;
+  //     std::cout << (int)x << std::endl;
+  //     std::cout << (int)y << std::endl;
 
-      Calc calc;
-      auto func = map.find(op);
-      // get string:function pair out of map
-      // call function
-      std::cout << map.find(op)->first << " = " << (calc.*(func->second))(2, 4) << std::endl;
-      // can't use strings in switch
-      // switch(op) {
-      //   case "+":
-      //     std::cout << first + second << std::endl;
-      //     break;
-      // }
-    }
-  }
+  //     Calc calc;
+  //     auto func = map.find(op);
+  //     // get string:function pair out of map
+  //     // call function
+  //     std::cout << map.find(op)->first << " = " << (calc.*(func->second))(2, 4) << std::endl;
+  //     // can't use strings in switch
+  //     // switch(op) {
+  //     //   case "+":
+  //     //     std::cout << first + second << std::endl;
+  //     //     break;
+  //     // }
+  //   }
+  // }
 
   return 0;
 }
