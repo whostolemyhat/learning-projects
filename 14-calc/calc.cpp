8af749b31109aaa80ee0de52c50bae8053dd1fe5
James Tease <james@jamestease.co.uk>
2016-08-04 10:14:16 +0100

    Starting calc cpp

--- 14-calc/calc.cpp
+++ 14-calc/calc.cpp
@@ -57,7 +57,7 @@ int main() {
       auto func = map.find(op);
       // get string:function pair out of map
       // call function
-      std::cout << map.find(op)->first << (calc.*(func->second))(x, y) << std::endl;
+      std::cout << map.find(op)->first << (calc.*(func->second))(2, 4) << std::endl;
       // can't use strings in switch
       // switch(op) {
       //   case "+":
