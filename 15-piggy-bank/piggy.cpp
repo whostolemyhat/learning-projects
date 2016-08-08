4ea843e0653381d9822a89bb2128f459635e1067
James Tease <james@jamestease.co.uk>
2016-08-08 10:49:45 +0100

    cpp piggy

---
+++ 15-piggy-bank/piggy.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+class Coin {
+private:
+  int amount;
+  int value;
+
+public:
+  Coin(int amount = 0, int value = 0) : amount(amount), area(area) {}
+
+  int total() {
+    return this.amount * this.value;
+  }
+}
+
+int main() {
+  std::vector<Coin> coins;
+  std::cout << "Enter number of coins: " << std::endl;
+
+  std::cout << "£2: ";
+  int two_pounds;
+  std::cin >> two_pounds;
+
+  if(!std::cin) {
+    std::cout << "Enter a number!" << std::endl;
+    return 1;
+  }
+
+  coins.push_back(Coin(two_pounds, 200));
+
+  return 0;
+}
