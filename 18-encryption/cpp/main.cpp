4101d0650edb7fdcf6c7153d94f2b79c3ccaa9a3
James Tease <james@jamestease.co.uk>
2016-08-19 11:51:47 +0100

    Starting cpp

--- 18-encryption/cpp/main.cpp
+++ 18-encryption/cpp/main.cpp
@@ -1,10 +1,27 @@
 // g++ -std=c++11 -Wall *.cpp -o output
 #include "rot13.h"
 #include <iostream>
+#include <fstream>
 
 int main(int argc, char* argv[]) {
+  if(argc == 0) {
+    std::cout << "Provide a file to convert." << std::endl;
+    return 0;
+  }
+
   for (int i = 0; i < argc; i++) {
     std::cout << argv[i] << std::endl;
+  }
+
+  std::string filename = argv[0];
+  if(fstream is(filename)) {
+    auto size = is.tellg(); // wtf
+    std::string str(size, '');
+    is.seekg(0);
+    is.read(&str[0], size);
+    std::cout << str << "\n";
+  } else {
+    std::cerr << "Couldn't open " << filename << std::endl;
   }
 
   std::cout << rot13("Hello World!") << std::endl;
