b39732f1251018bb968d4729f25d88d1e8c8869a
James Tease <james@jamestease.co.uk>
2016-08-19 10:46:14 +0100

    Starting cpp

--- 18-encryption/cpp/main.cpp
+++ 18-encryption/cpp/main.cpp
@@ -3,8 +3,8 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-  for (auto arg : argv) {
-    std::cout << argv << std::endl;
+  for (int i = 0; i < argc; i++) {
+    std::cout << argv[i] << std::endl;
   }
 
   std::cout << rot13("Hello World!") << std::endl;
