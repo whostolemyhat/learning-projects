5b0622d07b330c5c3c6215596762d47f2d1dde8f
James Tease <james@jamestease.co.uk>
2016-08-19 11:54:27 +0100

    Starting cpp

--- 18-encryption/cpp/main.cpp
+++ 18-encryption/cpp/main.cpp
@@ -5,15 +5,16 @@
 
 int main(int argc, char* argv[]) {
   if(argc == 0) {
-    std::cout << "Provide a file to convert." << std::endl;
-    return 0;
+    std::cerr << "Provide a file to convert.\nUsage: (programme) filename.txt" << std::endl;
+    return 1;
   }
 
   for (int i = 0; i < argc; i++) {
     std::cout << argv[i] << std::endl;
   }
 
-  std::string filename = argv[0];
+  std::string filename = argv[1];
+
   if(std::fstream is{filename}) {
     auto size = is.tellg(); // wtf
     std::string str(size, '\0');
