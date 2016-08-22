1ad11db18c9d0376133f87afa937d693fbf6f673
James Tease <james@jamestease.co.uk>
2016-08-22 15:18:25 +0100

    Start cpp

---
+++ 19-primes/cpp/primes.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+
+std::vector createSieve(int limit) {
+  std::vector primes = { true; limit };
+
+  return primes;
+}
+
+int main() {
+  int target = 32;
+
+  std::vector sieve = createSieve(target);
+
+  return 0;
+}
