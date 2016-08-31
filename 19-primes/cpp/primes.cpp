8707ee5e6f09a2537787f63994e66ced452ac3d2
James Tease <james@jamestease.co.uk>
2016-08-31 11:18:31 +0100

    Cpp primes

--- 19-primes/cpp/primes.cpp
+++ 19-primes/cpp/primes.cpp
@@ -8,7 +8,6 @@ int main() {
   std::cout << "Enter a number: ";
 
   int target;
-
   std::cin >> target;
 
   if(!std::cin) {
@@ -22,7 +21,6 @@ int main() {
   for(int i = 0; i < target / 2; i++) {
     if(sieve[i] == true) {
       if(sieve[target - i] == true) {
-        // std::pair<int, int> result(i, target - i);
         // emaplce_back adds straight into a pair in vec
         results.emplace_back(i, target - i);
       }
@@ -34,37 +32,37 @@ int main() {
   }
 
   // bit of testing :)
-  assert(sieve[2] == true);
-  assert(sieve[3] == true);
-  assert(sieve[4] == false);
-  assert(sieve[5] == true);
-  assert(sieve[6] == false);
-  assert(sieve[7] == true);
-  assert(sieve[8] == false);
-  assert(sieve[9] == false);
-  assert(sieve[10] == false);
-  assert(sieve[11] == true);
-  assert(sieve[12] == false);
-  assert(sieve[13] == true);
-  assert(sieve[14] == false);
-  assert(sieve[15] == false);
-  assert(sieve[16] == false);
-  assert(sieve[17] == true);
-  assert(sieve[18] == false);
-  assert(sieve[19] == true);
-  assert(sieve[20] == false);
-  assert(sieve[21] == false);
-  assert(sieve[22] == false);
-  assert(sieve[23] == true);
-  assert(sieve[24] == false);
-  assert(sieve[25] == false);
-  assert(sieve[26] == false);
-  assert(sieve[27] == false);
-  assert(sieve[28] == false);
-  assert(sieve[29] == true);
-  assert(sieve[30] == false);
-  assert(sieve[31] == true);
-  assert(sieve[32] == false);
+  // assert(sieve[2] == true);
+  // assert(sieve[3] == true);
+  // assert(sieve[4] == false);
+  // assert(sieve[5] == true);
+  // assert(sieve[6] == false);
+  // assert(sieve[7] == true);
+  // assert(sieve[8] == false);
+  // assert(sieve[9] == false);
+  // assert(sieve[10] == false);
+  // assert(sieve[11] == true);
+  // assert(sieve[12] == false);
+  // assert(sieve[13] == true);
+  // assert(sieve[14] == false);
+  // assert(sieve[15] == false);
+  // assert(sieve[16] == false);
+  // assert(sieve[17] == true);
+  // assert(sieve[18] == false);
+  // assert(sieve[19] == true);
+  // assert(sieve[20] == false);
+  // assert(sieve[21] == false);
+  // assert(sieve[22] == false);
+  // assert(sieve[23] == true);
+  // assert(sieve[24] == false);
+  // assert(sieve[25] == false);
+  // assert(sieve[26] == false);
+  // assert(sieve[27] == false);
+  // assert(sieve[28] == false);
+  // assert(sieve[29] == true);
+  // assert(sieve[30] == false);
+  // assert(sieve[31] == true);
+  // assert(sieve[32] == false);
 
   return 0;
 }
