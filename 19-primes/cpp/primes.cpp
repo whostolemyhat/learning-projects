e370c0e0306021b474d94581217dfc0ab665abee
James Tease <james@jamestease.co.uk>
2016-08-31 10:52:49 +0100

    Cpp primes

--- 19-primes/cpp/primes.cpp
+++ 19-primes/cpp/primes.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 
+#include <cassert>
+
 std::vector<bool> createSieve(int limit) {
   std::vector<bool> primes(limit + 1, true);
 
@@ -13,7 +15,7 @@ std::vector<bool> createSieve(int limit) {
       int step = i;
       int n = i * i;
 
-      while(n < limit) {
+      while(n <= limit ) {
         primes[n] = false;
         n += step;
       }
@@ -33,6 +35,38 @@ int main() {
   for(auto item : sieve) {
     std::cout << item << std::endl;
   }
+
+  assert(sieve[2] == true);
+  assert(sieve[3] == true);
+  assert(sieve[4] == false);
+  assert(sieve[5] == true);
+  assert(sieve[6] == false);
+  assert(sieve[7] == true);
+  assert(sieve[8] == false);
+  assert(sieve[9] == false);
+  assert(sieve[10] == false);
+  assert(sieve[11] == true);
+  assert(sieve[12] == false);
+  assert(sieve[13] == true);
+  assert(sieve[14] == false);
+  assert(sieve[15] == false);
+  assert(sieve[16] == false);
+  assert(sieve[17] == true);
+  assert(sieve[18] == false);
+  assert(sieve[19] == true);
+  assert(sieve[20] == false);
+  assert(sieve[21] == false);
+  assert(sieve[22] == false);
+  assert(sieve[23] == true);
+  assert(sieve[24] == false);
+  assert(sieve[25] == false);
+  assert(sieve[26] == false);
+  assert(sieve[27] == false);
+  assert(sieve[28] == false);
+  assert(sieve[29] == true);
+  assert(sieve[30] == false);
+  assert(sieve[31] == true);
+  assert(sieve[32] == false);
 
   return 0;
 }
