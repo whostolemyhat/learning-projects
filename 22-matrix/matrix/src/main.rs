28193e8c13c1779dac4c6b249c3ccbdf07045864
James Tease <james@jamestease.co.uk>
2016-09-21 14:13:36 +0100

    operations

--- 22-matrix/matrix/src/main.rs
+++ 22-matrix/matrix/src/main.rs
@@ -26,22 +26,38 @@ impl<T> Matrix<T> where T: Copy {
     Matrix { rows: rows, cols: cols, matrix: _rows }
   }
 
-  // fn new_random(rows: i32, cols: i32) -> Self {
-  //   let mut _rows: Vec<Vec<i32>> = Vec::new();
+  fn new_from_vec(data: Vec<Vec<T>>) -> Self {
+    let rows = data.len() as i32;
+    let cols = data[0].len() as i32;
 
-  //   for _ in 0..rows {
-  //     let mut _row: Vec<i32> = Vec::new();
+    Matrix { rows: rows, cols: cols, matrix: data }
+  }
 
-  //     for _ in 0..cols {
-  //       let num: i32 = rand::thread_rng().gen_range(-20, 20);
-  //       _row.push(num);
-  //     }
+  fn scalar_mul(self, multiplier: T) -> Self where T: Mul<Output=T> + Copy + Default {
+    let mut new_matrix = Matrix::new(self.rows, self.cols, Default::default());
 
-  //     _rows.push(_row);
-  //   }
+    for i in 0..self.rows {
+      let row = i as usize;
 
-  //   Matrix { rows: rows, cols: cols, matrix: _rows }
-  // }
+      for j in 0..self.cols {
+        let col = j as usize;
+
+        new_matrix.matrix[row][col] = self.matrix[row][col] * multiplier;
+      }
+    }
+
+    new_matrix
+  }
+
+  fn dot(self, other: Matrix<T>) -> Self {
+    for i in 0..self.rows {
+      for j in 0..self.cols {
+
+      }
+    }
+
+    self
+  }
 }
 
 // Make sure generic type T implements Add (so you can add them together)
@@ -86,18 +102,24 @@ impl<T> Sub for Matrix<T> where T: Sub<Output=T> + Copy + Default {
   }
 }
 
-impl<T> Mul for Matrix<T> where T: Mul<Output=T> + Copy + Default {
+impl<T> Mul for Matrix<T> where T: Mul<Output=T> + Copy + Default + Add<Output=T> {
   type Output = Matrix<T>;
 
   fn mul(self, other: Matrix<T>) -> Matrix<T> {
-    // http://stattrek.com/matrix-algebra/matrix-multiplication.aspx
-    let mut new_matrix: Matrix<T> = Matrix::new(other.rows, other.cols, Default::default());
+    // other.rows must eq self.cols
+    // size = self.rows x other.cols
 
-    for i in 0..other.rows {
+    // http://www.freemathhelp.com/matrix-multiplication.html
+    let mut new_matrix: Matrix<T> = Matrix::new(self.rows, other.cols, Default::default());
+
+    for i in 0..self.rows {
       for j in 0..other.cols {
+        let mut total: T = Default::default();
         for k in 0..other.rows {
-          new_matrix.matrix[i as usize][j as usize] = self.matrix[i as usize][k as usize] * other.matrix[k as usize][j as usize];
+          total = total + self.matrix[i as usize][k as usize] * other.matrix[k as usize][j as usize];
         }
+        // each number in self.row * each number in other.col
+        new_matrix.matrix[i as usize][j as usize] = total;
       }
     }
 
@@ -139,15 +161,14 @@ fn random_matrix(rows: i32, cols: i32) -> Matrix<i32> {
   Matrix { rows: rows, cols: cols, matrix: _rows }
 }
 
-
 fn main() {
-  println!("{}", random_matrix(3, 3));
-  println!("{}", random_matrix(3, 6));
-  println!("{}", random_matrix(2, 2));
+  // println!("{}", random_matrix(3, 3));
+  // println!("{}", random_matrix(3, 6));
+  // println!("{}", random_matrix(2, 2));
 
-  println!("{}", Matrix::new(3, 3, 1));
-  println!("{}", Matrix::new(3, 6, 0));
-  println!("{}", Matrix::new(2, 2, 2));
+  // println!("{}", Matrix::new(3, 3, 1));
+  // println!("{}", Matrix::new(3, 6, 0));
+  // println!("{}", Matrix::new(2, 2, 2));
 
   let first = Matrix::new(3, 3, 1);
   let mut second = Matrix::new(3, 3, 2);
@@ -159,10 +180,59 @@ fn main() {
   let fourth = Matrix::new(3, 3, 4);
   println!("{}", third - fourth);
 
-  let fifth = Matrix::new(4, 4, 5);
-  let sixth = Matrix::new(3, 6, 8);
+  let mut fifth = Matrix::new(2, 3, 0);
+  fifth.matrix[0][1] = 1;
+  fifth.matrix[0][2] = 2;
+  fifth.matrix[1][0] = 3;
+  fifth.matrix[1][1] = 4;
+  fifth.matrix[1][2] = 5;
+
+  let mut sixth = Matrix::new(3, 2, 6);
+  sixth.matrix[0][1] = 7;
+  sixth.matrix[1][0] = 8;
+  sixth.matrix[1][1] = 9;
+  sixth.matrix[2][0] = 10;
+  sixth.matrix[2][1] = 11;
+
   println!("{}", fifth * sixth);
+
+  let this_matrix = vec![
+    vec![1, 2, 3],
+    vec![4, 5, 6]
+  ];
+  let this_one = Matrix::new_from_vec(this_matrix);
+
+  let another_matrix = vec![
+    vec![9],
+    vec![8],
+    vec![7]
+  ];
+  let another = Matrix::new_from_vec(another_matrix);
+
+  println!("{}", this_one * another);
+
+  let mut same_one = Matrix::new(3, 1, 1);
+  same_one.matrix[1][0] = 2;
+  same_one.matrix[2][0] = 3;
+
+  let mut same_two = Matrix::new(3, 1, 4);
+  same_two.matrix[1][0] = 5;
+  same_two.matrix[2][0] = 6;
+
+  println!("{}", same_one.dot(same_two));
+
+  let seventh = Matrix::new(4, 5, 7);
+  println!("{}", seventh.scalar_mul(3));
+
+  let matrix = vec![
+    vec![13, 9, 7, 15],
+    vec![8, 7, 4, 6],
+    vec![6, 4, 0, 3]
+  ];
+  let second = Matrix::new_from_vec(matrix);
+  println!("{}", second);
 }
+
 
 #[cfg(test)]
 mod test {
@@ -191,6 +261,20 @@ mod test {
     let ran = random_matrix(2, 5);
     assert_eq!(ran.rows, 2);
     assert_eq!(ran.cols, 5);
+
+    let third_matrix = vec![
+      vec![13, 9, 7, 15],
+      vec![8, 7, 4, 6],
+      vec![6, 4, 0, 3]
+    ];
+    let third = Matrix::new_from_vec(vec![
+      vec![13, 9, 7, 15],
+      vec![8, 7, 4, 6],
+      vec![6, 4, 0, 3]
+    ]);
+    assert_eq!(third.rows, 3);
+    assert_eq!(third.cols, 4);
+    assert_eq!(third.matrix, third_matrix);
   }
 
   #[test]
@@ -215,5 +299,71 @@ mod test {
     assert_eq!(third.rows, 3);
     assert_eq!(third.cols, 3);
     assert_eq!(third.matrix, matrix);
+  }
+
+  #[test]
+  fn mul() {
+    let first = Matrix::new(3, 3, 2);
+    let second = Matrix::new(3, 3, 3);
+    let third = first * second;
+    let matrix = vec![vec![18, 18, 18], vec![18, 18, 18], vec![18, 18, 18]];
+
+    assert_eq!(third.matrix, matrix);
+
+    let mut fifth = Matrix::new(2, 2, 5);
+    fifth.matrix[0][0] = 1;
+    fifth.matrix[0][1] = 6;
+    fifth.matrix[1][0] = 3;
+    fifth.matrix[1][1] = 8;
+
+    let mut sixth = Matrix::new(2, 2, 8);
+    sixth.matrix[0][0] = 2;
+    sixth.matrix[0][1] = 2;
+    sixth.matrix[1][0] = 9;
+    sixth.matrix[1][1] = 7;
+
+    let second_matrix = vec![vec![56, 44], vec![78, 62]];
+    assert_eq!((fifth * sixth).matrix, second_matrix);
+
+    let mut this_one = Matrix::new(2, 3, 1);
+    this_one.matrix[0][1] = 2;
+    this_one.matrix[0][2] = 3;
+    this_one.matrix[1][0] = 4;
+    this_one.matrix[1][1] = 5;
+    this_one.matrix[1][2] = 6;
+
+    let mut another = Matrix::new(3, 1, 9);
+    another.matrix[1][0] = 8;
+    another.matrix[2][0] = 7;
+
+    let third_matrix = vec![vec![46], vec![118]];
+    assert_eq!((this_one * another).matrix, third_matrix);
+  }
+
+  #[test]
+  fn scalar_mul() {
+    let first = Matrix::new(3, 3, 2);
+    let second = first.scalar_mul(3);
+    let matrix = vec![vec![6, 6, 6], vec![6, 6, 6], vec![6, 6, 6]];
+
+    assert_eq!(second.matrix, matrix);
+
+    let third = Matrix::new(4, 2, 3.0);
+    let fourth = third.scalar_mul(0.5);
+    let second_matrix = vec![vec![1.5, 1.5], vec![1.5, 1.5], vec![1.5, 1.5], vec![1.5, 1.5]];
+
+    assert_eq!(fourth.matrix, second_matrix);
+  }
+
+  #[test]
+  fn dot() {
+    // [3 4 2] x [13 9 7 15 = [83 63 37 75] (83 = 3x13 + 4x8 + 2x6)
+    //             8 7 4 6
+    //             6 4 0 3]
+    let mut first = Matrix::new(1, 3, 3);
+    first.matrix[0][1] = 4;
+    first.matrix[0][2] = 2;
+
+
   }
 }
