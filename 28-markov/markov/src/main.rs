54af145173b6c8ac1a0af0d306d508271bfddae3
James Tease <james.tease@replgroup.com>
2017-04-24 16:43:42 +0100

    Better total length fn

--- 28-markov/markov/src/main.rs
+++ 28-markov/markov/src/main.rs
@@ -22,29 +22,45 @@ fn open_file(filename: &Path) -> String {
     content
 }
 
+fn strip(text: &String) -> String {
+    let no_punctuation = Regex::new(r"[^a-zA-Z0-9]").unwrap();
+
+    no_punctuation.replace_all(&text, " ").to_string()
+}
+
+fn create_histogram(text: &String) -> HashMap<String, u32> {
+    let mut histogram: HashMap<String, u32> = HashMap::new();
+
+    for word in strip(&text).split(" ").filter(|s| !s.is_empty()) {
+        *histogram.entry(word.to_lowercase()).or_insert(0) += 1;
+    }
+
+    histogram
+}
+
+fn get_total_words(histogram: &HashMap<String, u32>) -> u32 {
+    histogram.iter().fold(0, |acc, words| acc + words.1)
+}
+
+// fn keyword_occurences(word: String, histogram: HashMap<String, u32>) -> Result<Some, None> {
+//     histogram.get(word)
+// }
+
 fn main() {
     // read file or whatever
     let text = open_file(&Path::new("./emma.txt"));
 
-    let no_punctuation = Regex::new(r"[^a-zA-Z0-9]").unwrap();
-    let replaced = no_punctuation.replace_all(&text, " ");
-    let mut total_words = 0;
-
-    let mut histogram: HashMap<String, u32> = HashMap::new();
-    for word in replaced.split(" ").filter(|s| !s.is_empty()) {
-        *histogram.entry(word.to_lowercase()).or_insert(0) += 1;
-        total_words += 1;
-    }
-
+    let histogram = create_histogram(&text);
     let mut sorted: Vec<_> = histogram.iter().collect();
     sorted.sort_by(|a, b| b.1.cmp(a.1));
-    // println!("{:?}", sorted);
+    let total_words = get_total_words(&histogram);
 
+    println!("Occurences: {:?} ({}%)", histogram.get("emma"), *histogram.get("emma").unwrap() as f32 / total_words as f32);
     println!("Total words: {:?}", total_words);
     println!("Unique words: {}", sorted.len());
 
     println!("Most used words:");
-    for i in 0..20 {
+    for i in 0..30 {
         println!("{}: {}", sorted[i].0, sorted[i].1);
     }
 }
